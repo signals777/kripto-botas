eb93d45e81124865e23902555cd6d69a6ac7c51a
signals777 <virglel@gmail.com>
2025-07-03 02:05:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def demo_trade_bot():
                 "pelnas": result,
                 "balansas": round(balance, 2),
                 "procentai": round(result_pct, 2)
-datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                'laikas': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             })
             
             time.sleep(0.1)  # Demo greitis, realiai intervalas ilgesnis
